4519c926cbe1da44021d3ae4f026c610b8af8eaa
Ivoline Ngong <ivolinengong@gmail.com>
2017-09-28 14:30:41 +0100

    Update Utils.java

--- PopularMoviesStage1/app/src/main/java/ivy/com/popularmoviesstage1/util/Utils.java
+++ PopularMoviesStage1/app/src/main/java/ivy/com/popularmoviesstage1/util/Utils.java
@@ -5,8 +5,8 @@ package ivy.com.popularmoviesstage1.util;
  */
 public class Utils {
     public static final String mUrl = "https://image.tmdb.org/t/p/w185_and_h278_bestv2";
-    public static final String URL_POP = "http://api.themoviedb.org/3/movie/popular?api_key==[YOUR_API_KEY]";
-    public static final String URL_RATED = "http://api.themoviedb.org/3/movie/top_rated?api_key==[YOUR_API_KEY]";
+    public static final String URL_POP = "http://api.themoviedb.org/3/movie/popular?api_key=[YOUR_API_KEY]";
+    public static final String URL_RATED = "http://api.themoviedb.org/3/movie/top_rated?api_key=[YOUR_API_KEY]";
     public static final String TAG = "Log: ";
 
 }

8c21e8172be2863d740458cd6c308136e8cc1939
Ngong Ivoline Clarisse <ivolinengong@gmail.com>
2020-03-31 20:04:36 +0300

    added data binding for welcome screen

--- Shoe Store-Kotlin/app/src/main/java/com/udacity/shoestore/WelcomeFragment.kt
+++ Shoe Store-Kotlin/app/src/main/java/com/udacity/shoestore/WelcomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.databinding.FragmentWelcomeBinding
 
@@ -23,11 +24,13 @@ class WelcomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         welcomeBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_welcome, container, false )
-
-        welcomeBinding.nextButton.setOnClickListener{
-            findNavController().navigate(R.id.action_welcomeFragment_to_instructionsFragment)
-        }
+        
         return welcomeBinding.root
+    }
+
+    fun navigate(view: View){
+        val action = WelcomeFragmentDirections.actionWelcomeFragmentToInstructionsFragment()
+        view.findNavController().navigate(action)
     }
 
 }
